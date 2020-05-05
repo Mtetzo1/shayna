05182a81590a224b623788d801069d0d904c265d
Mr. Ardian <marshaardianperdana1@gmail.com>
2020-05-05 11:11:34 +0700

    UPDATE SHAYNA

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Produk from '../views/Produk.vue'
+import cart from '../views/ShoppingCart.vue'
+import Success from '../views/Success.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/produk',
+    name: 'Produk',
+    component: Produk
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: cart
+  },
+  {
+    path: '/success',
+    name: 'Success',
+    component: Success
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
